7ac7409864c931f7d6ab188bc315510041c3bf44
Simiethro <simiethroscott@gmail.com>
2025-02-17 19:29:51 +0100

    test commits

--- app/views/homepages/index.php
+++ app/views/homepages/index.php
@@ -10,7 +10,7 @@
 
             <h3><?php echo $data['title']; ?></h3>
 
-            <a href="<?= URLROOT; ?>/smartphones/index ">Toon smartphones</a>
+            <a href="<?= URLROOT; ?>/smartphones/index ">Toon smartphones TEST</a>
 
         </div>
         
