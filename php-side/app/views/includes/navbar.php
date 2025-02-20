8e53531a08575c4c96f988328e3b979ac1b932ec
ddylvianen <87158436+ddylvianen@users.noreply.github.com>
2025-02-20 13:51:11 +0100

    made navbar responsive

--- php-side/app/views/includes/navbar.php
+++ php-side/app/views/includes/navbar.php
@@ -3,13 +3,13 @@
     <a href="/">
       <h1 class="logo">FitForFun</h1>
     </a>
-    <div class="hamburger-menu">
-      <i class="fa-solid fa-bars" id="hamburger-menu"></i>
+    <div id="hamburger-menu">
+      <i class="fa-solid fa-bars"></i>
     </div>
   </div>
-  <div class="nav-links-cont">
+  <div class="nav-links-cont open">
     
-    <div class="nav-links">
+    <div class="nav-links open">
       <a href="/">home</a>
       <a href="/lessen">lessen</a>
       <a href="/reservering">reserveringen</a>
@@ -17,9 +17,23 @@
       <a href="/overzicht">medewerkerlessen</a>
     </div>
 
-    <div class="nav-links nav-account">
+    <div class="nav-links nav-account open">
       <a href="">Sign up</a>
       <a href="">Login</a>
     </div>
   </div>
 </nav>
+
+<script>
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const navLinksCont = document.querySelector('.nav-links-cont');
+  const navLinks = document.querySelectorAll('.nav-links');
+
+  console.log(navLinks);
+
+  hamburgerMenu.addEventListener('click', () => {
+    navLinksCont.classList.toggle('open');
+    navLinks.forEach(i=> {i.classList.toggle('open');});
+    // navLinks.classList.toggle('open');
+  });
+</script>
--- php-side/app/views/includes/pricing.php
+++ php-side/app/views/includes/pricing.php
@@ -1,39 +1,45 @@
 <link rel="stylesheet" href="../public/css/pricing.css">
 
 <div class="pricingTable">
-  <h2 class="pricingTable-title">Find a plan that's right for you.</h2>
-  <h3 class="pricingTable-subtitle">Every plan comes with a 30-day   free trial.</h3>
+  <h2 class="pricingTable-title">Kies een plan dat bij jou past.</h2>
+  <h3 class="pricingTable-subtitle">Elk plan komt met een gratis proefperiode van 30 dagen.</h3>
   
   <ul class="pricingTable-firstTable">
     <li class="pricingTable-firstTable_table">
-      <h1 class="pricingTable-firstTable_table__header">Bronze Packege</h1>
-      <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>10</span><span>Month</span></p>
+      <h1 class="pricingTable-firstTable_table__header">Bronze Pakket</h1>
+      <p class="pricingTable-firstTable_table__pricing"><span>€</span><span>10</span><span>Per Maand</span></p>
       <ul class="pricingTable-firstTable_table__options">
-        <li>Unlimited Listing</li>
-        <li>Edit Your Listing</li>
-        <li>Approve Reviews</li>
+        <li>Onbeperkt Toegang tot Zwemmen</li>
+        <li>Gebruik van Fitnessapparatuur</li>
+        <li>Deelname aan Groepslessen</li>
+        <li class="empty-option"></li>
+        <li class="empty-option"></li>
       </ul>
-      <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
-    </li><li class="pricingTable-firstTable_table">
-      <h1 class="pricingTable-firstTable_table__header">Gold Packege</h1>
-      <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>19</span><span>Month</span></p>
+      <button class="pricingTable-firstTable_table__getstart">Begin Nu</button>
+    </li>
+    <li class="pricingTable-firstTable_table">
+      <h1 class="pricingTable-firstTable_table__header">Goud Pakket</h1>
+      <p class="pricingTable-firstTable_table__pricing"><span>€</span><span>19</span><span>Per Maand</span></p>
       <ul class="pricingTable-firstTable_table__options">
-        <li>Unlimited Listing</li>
-        <li>Edit Your Listing</li>
-        <li>Approve Reviews</li>
-        <li>Take Booking Online</li>
-        <li>24/7 Support Service</li>
+        <li>Onbeperkt Toegang tot Zwemmen</li>
+        <li>Gebruik van Fitnessapparatuur</li>
+        <li>Deelname aan Groepslessen</li>
+        <li>Gratis Squash en Tennis</li>
+        <li>24/7 Klantenservice</li>
       </ul>
-      <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
-    </li><li class="pricingTable-firstTable_table">
-      <h1 class="pricingTable-firstTable_table__header">Platinum Packege</h1>
-      <p class="pricingTable-firstTable_table__pricing"><span>$</span><span>49</span><span>Month</span></p>
+      <button class="pricingTable-firstTable_table__getstart">Begin Nu</button>
+    </li>
+    <li class="pricingTable-firstTable_table">
+      <h1 class="pricingTable-firstTable_table__header">Platinum Pakket</h1>
+      <p class="pricingTable-firstTable_table__pricing"><span>€</span><span>49</span><span>Per Maand</span></p>
       <ul class="pricingTable-firstTable_table__options">
-        <li>Unlimited Listing</li>
-        <li>Edit Your Listing</li>
-        <li>Approve Reviews</li>
+        <li>Onbeperkt Toegang tot Zwemmen</li>
+        <li>Gebruik van Fitnessapparatuur</li>
+        <li>Deelname aan Groepslessen</li>
+        <li>Gratis Squash en Tennis</li>
+        <li>Persoonlijke Trainer Sessies</li>
       </ul>
-      <button class="pricingTable-firstTable_table__getstart">Get Started Now</button>
+      <button class="pricingTable-firstTable_table__getstart">Begin Nu</button>
     </li>
   </ul>
 </div>
